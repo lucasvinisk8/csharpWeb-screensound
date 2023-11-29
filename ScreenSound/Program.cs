7d17881d51fd1b1a7dfff7980a36fcfd2353de12
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-11-29 19:22:35 -0300

    video 1.3 - conectando com o BD

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,5 +1,20 @@
-﻿using ScreenSound.Menus;
+﻿using ScreenSound.Banco;
+using ScreenSound.Menus;
 using ScreenSound.Modelos;
+
+try
+{
+    using var connection = new Connection().ObterConexao();
+    connection.Open();
+    Console.WriteLine(connection.State);
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
+
+return;
 
 Artista ira = new Artista("Ira!", "Banda Ira!");
 Artista beatles = new("The Beatles", "Banda The Beatles");

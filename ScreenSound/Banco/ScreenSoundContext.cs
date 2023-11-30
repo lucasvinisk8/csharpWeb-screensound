82f674804e090cdf309c3b85a5df4ac9e06906a9
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-11-30 01:02:15 -0300

    video 2.2 - mapeando artistas

--- ScreenSound/Banco/ScreenSoundContext.cs
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 namespace ScreenSound.Banco;
 internal class ScreenSoundContext: DbContext
 {
+    public DbSet<Artista> Artistas { get; set; }
+
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)

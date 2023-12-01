fdb8ad33e0cefc9ed0265a1f81bf187d82d88c02
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-12-01 02:18:13 -0300

    3.3 - utilizando Func

--- ScreenSound/Banco/DAL.cs
+++ ScreenSound/Banco/DAL.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ScreenSound.Banco;
-internal abstract class DAL<T> where T : class
+internal class DAL<T> where T : class
 {
     protected readonly ScreenSoundContext context;
 
@@ -32,5 +32,10 @@ internal abstract class DAL<T> where T : class
     {
         context.Set<T>().Remove(objeto);
         context.SaveChanges();
+    }
+
+    public T? RecuperarPor(Func<T, bool> condicao)
+    {
+        return context.Set<T>().FirstOrDefault(condicao);
     }
 }

c5c79a66d8f5f2d57e0f5004a1c3818bb00273f8
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-12-01 02:12:03 -0300

    3.2 - implementando métodos genéricos

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -9,36 +9,7 @@ using System.Threading.Tasks;
 namespace ScreenSound.Banco;
 internal class ArtistaDAL: DAL<Artista>
 {
-    private readonly ScreenSoundContext context;
-
-    public ArtistaDAL(ScreenSoundContext context)
-    {
-        this.context = context;
-    }
-
-    public override IEnumerable<Artista> Listar()
-    {
-        return context.Artistas.ToList();
-    }
-
-    public override void Adicionar(Artista artista)
-    {
-        context.Artistas.Add(artista);
-        context.SaveChanges();
-
-    }
-
-    public override void Atualizar(Artista artista)
-    {
-        context.Artistas.Update(artista);
-        context.SaveChanges();
-    }
-
-    public override void Deletar(Artista artista)
-    {
-        context.Artistas.Remove(artista);
-        context.SaveChanges();
-    }
+    public ArtistaDAL(ScreenSoundContext context) : base(context) { }
 
     public Artista? RecuperarPeloNome(string nome)
     {
--- ScreenSound/Banco/DAL.cs
+++ ScreenSound/Banco/DAL.cs
@@ -5,10 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ScreenSound.Banco;
-internal abstract class DAL<T>
+internal abstract class DAL<T> where T : class
 {
-    public abstract IEnumerable<T> Listar();
-    public abstract void Adicionar(T objeto);
-    public abstract void Atualizar(T objeto);
-    public abstract void Deletar(T objeto);
+    protected readonly ScreenSoundContext context;
+
+    protected DAL(ScreenSoundContext context)
+    {
+        this.context = context;
+    }
+
+    public IEnumerable<T> Listar()
+    {
+        return context.Set<T>().ToList();
+    }
+    public void Adicionar(T objeto)
+    {
+        context.Set<T>().Add(objeto);
+        context.SaveChanges();
+    }
+    public void Atualizar(T objeto)
+    {
+        context.Set<T>().Update(objeto);
+        context.SaveChanges();
+    }
+    public void Deletar(T objeto)
+    {
+        context.Set<T>().Remove(objeto);
+        context.SaveChanges();
+    }
 }
